d6f2d7edad3dfe23d736f0bc2bba0ecce0a336c5
Ilase <igeodec@gmail.com>
2024-11-01 18:09:24 +0300

    #6 sdsd

---
+++ include/rnt-sys.hpp
@@ -0,0 +1,17 @@
+#ifndef _RNT_SYS_HPP
+#define _RNT_SYS_HPP
+#include "rnt.hpp"
+namespace rnt{
+
+    struct DataField{
+        
+    };
+
+    class Data{
+        std::vector<std::string> bus_id;
+
+    };
+
+}
+
+#endif
